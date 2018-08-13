d96d33b3ec30c48683d39b59c37dfabac121f0e0
Tien Pham <r3dsky02@yahoo.com.vn>
2018-08-14 00:16:36 +0300

    Organizing Our Resolvers

---
+++ apollo/imports/api/resolutions/resolvers.js
@@ -0,0 +1,16 @@
+export default {
+  Query: {
+    resolutions() {
+      return [
+        {
+          _id: "asfasffsd",
+          name: "Get stuff done!"
+        },
+        {
+          _id: "asfasffsffd",
+          name: "Lose some weight!"
+        },
+      ];
+    }
+  }
+};
--- apollo/imports/startup/server/register-api.js
+++ apollo/imports/startup/server/register-api.js
@@ -1,7 +1,9 @@
 import { createApolloServer } from "meteor/apollo";
 import { makeExecutableSchema } from "graphql-tools";
+import { merge } from "lodash";
 
 import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
+import ResolutionsResolvers from "../../api/resolutions/resolvers";
 
 const testSchema = `
   type Query {
@@ -12,25 +14,15 @@ const testSchema = `
 
 const typeDefs = [testSchema, ResolutionsSchema];
 
-const resolvers = {
+const resolver = {
   Query: {
     hi() {
       return "Hello level up";
-    },
-    resolutions() {
-      return [
-        {
-          _id: "asfasffsd",
-          name: "Get stuff done!"
-        },
-        {
-          _id: "asfasffsffd",
-          name: "Lose some weight!"
-        },
-      ];
     }
   }
 };
+
+const resolvers = merge(resolver, ResolutionsResolvers);
 
 const schema = makeExecutableSchema({
   typeDefs,
